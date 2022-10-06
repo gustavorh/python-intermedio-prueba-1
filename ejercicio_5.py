1439626bcfd01decc3c265bc3b2fa38b1a91fa3a
Gustavo <gustavocars@gmail.com>
2022-10-05 21:44:18 -0300

    Actualizado Ejercicio 5

--- ejercicio_5.py
+++ ejercicio_5.py
@@ -1,19 +1,25 @@
 def main():
     valor = int(input("Ingrese el valor de la factura: "))
-    iva = int(input("Ingrese el porcentaje de IVA a aplicar (0 a 100): "))
+    iva = input("Ingrese el porcentaje de IVA a aplicar (0 a 100): ")
 
     factura_iva(valor, iva)
 
 def factura_iva(valor, iva):
-    if iva == 0:
+    if not iva:
+        iva = 0
+        iva = int(iva)
+        
         iva = 19 / 100
         factura = valor + (valor * iva)
     else:
+        iva = int(iva)
+        print(f"IVA ingresado {iva}")
         iva = iva / 100
         factura = valor + (valor * iva)
 
     print(f"El valor de la factura original es de {valor}, aplicando un IVA de {iva * 100}%, el valor de la factura con IVA es de: {factura}")
     
     return factura
+    
 if __name__ == '__main__':
     main()
